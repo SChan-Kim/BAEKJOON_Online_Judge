ea5da8a01d1d294812348d3392b3fbb0b1c216c5
SChan-Kim <instufld@gmail.com>
2020-04-30 12:12:44 +0900

    입출력과 사칙연산-3,4

--- Playground.cpp
+++ Playground.cpp
@@ -4,31 +4,11 @@ using namespace std;
 
 int main() {
 
-    int a,b,c,result;
-
-    cin >> a >> b >> c;
-
-    result = 0;
-
-    while(1) {
-        if(b>c) {
-            cout << "-1" << endl;
-            break;
-        }
-
-        if(result*c > a+(b*result)) {
-            if(result == 0)
-                cout << "-1" << endl;
-            else
-                cout << result << endl;
-            break;
-        }
-
-        else
-            result++;
-    }
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"\`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
 
     return 0;
 }
-
-// 손익분기점이 달성되지 않는 경우를 찾아야함.
