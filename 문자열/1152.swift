d9d183f02407f582159512888a0bd607bfe586de
SChan-Kim <63276842+SChan-Kim@users.noreply.github.com>
2021-01-27 22:44:44 +0900

    Delete 중복 폴더

--- 문자열/1152.swift
+++
@@ -1,9 +0,0 @@
-let input = readLine()!.split(separator: " ").map{ Int($0)! }
-let fir = (input[0] / 100) + (((input[0] / 10) % 10) * 10) + ((input[0] % 10) * 100)
-let sec = (input[1] / 100) + (((input[1] / 10) % 10) * 10) + ((input[1] % 10) * 100)
-
-if fir > sec {
-    print(fir)
-} else {
-    print(sec)
-}
--- 문자열/11720.swift
+++
@@ -1,9 +0,0 @@
-let num = readLine()!
-var input = readLine()!
-var result = 0
-
-for char in input {
-    result += Int(String(char))!
-}
-
-print(result)
